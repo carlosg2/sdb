cd84f2b048208b3611dedcb0404c78843b94f1c1
carlosg2 <carlosg2@gmail.com>
2022-10-20 05:59:30 +0000

    update

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,9 +9,9 @@
 
 <main class="mx-auto max-w-screen-lg relative break-normal">
 
-  <h1 class="font-bold mb-5 text-5xl text-center">About</h1>
+  <h1 class="font-bold mb-5 text-5xl text-center">Nuestra Historia</h1>
   <div class="mb-10 all-prose">
-    <svelte:component this={copy} />
+    <!-- <svelte:component this={copy} /> -->
   </div>
 
   <div class="flex flex-col mb-20 w-full">
--- src/routes/sucursales/+page.svelte
+++ src/routes/sucursales/+page.svelte
@@ -29,7 +29,7 @@
     {#each posts as post, index} 
       {#if post.published}
         <li>
-          <a href="#{post.title}" class:btn-primary="{scrollIndex === index}" class="btn btn-sm  md:btn-lg mr-2">ok</a>  
+          <a href="#{post.title}" class:btn-primary="{scrollIndex === index}" class="btn btn-sm  md:btn-lg mr-2">{post.title}</a>  
         </li>
       {/if}
       
@@ -44,9 +44,39 @@
   <div class="flex flex-col flex-grow"> 
     
     {#each posts as post}
-      {#if post.published}
-        <PostCard {post} pre="sucursales" />
-      {/if}
+      
+    <div class="flex px-4 items-start border-[0.5px] border-base-300 bg-base-100 rounded-2xl select-none" >
+            
+      <div class=" w-full relative py-4">
+        
+        <div class="flex justify-between items-center ">
+          <div class="shrink tracking-wide font-semibold leading-5 text-base md:text-xl">
+             {post.title}
+          </div>
+        </div>
+        
+        <div class="font-semibold tracking-wide flex items-center space-x-1 opacity-80" >
+          $ {post.title}
+        </div>
+
+        <div class="text-base md:text-base line-clamp-2 opacity-60">
+          {post.title}
+        </div>
+
+        
+
+      </div>
+
+      <div class="ml-4 shrink-0 py-4">
+        <!-- <div
+          class="rounded-lg w-20 h-20 md:w-36 md:h-28 bg-cover bg-center"  
+          style="background-image: url(assets/images/{post.title});"
+          alt="demo"
+        /> -->
+      </div>
+
+    </div>
+    
     {/each}
   </div>
 
