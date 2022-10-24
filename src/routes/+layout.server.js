9e1d7ab6f1263e1b4e5fe4920f0a2b5fec46b941
carlosg2 <carlosg2@gmail.com>
2022-10-24 21:48:03 +0000

    update

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -2,12 +2,6 @@
 /** @type {import('./$types').PageServerLoad} */
 export function load(event) {
 	return {
-		ip: event.getClientAddress(),
-		city: decodeURIComponent(/** @type {string} */ (event.request.headers.get('x-vercel-ip-city'))),
-        country: decodeURIComponent(/** @type {string} */ (event.request.headers.get('x-vercel-ip-country'))),
-        region: decodeURIComponent(/** @type {string} */ (event.request.headers.get('x-vercel-ip-country-region'))),
-        latitude: decodeURIComponent(/** @type {string} */ (event.request.headers.get('x-vercel-ip-latitude'))),
-        longitude: decodeURIComponent(/** @type {string} */ (event.request.headers.get('x-vercel-ip-longitude'))),
-        timezone: decodeURIComponent(/** @type {string} */ (event.request.headers.get('x-vercel-ip-timezone')))
+		
 	};
 }
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -12,6 +12,13 @@ export const load = async ({ fetch, params }) => {
     return {
       sucursales: fetchProducts(),
       slug: params.slug
+      ip: event.getClientAddress(),
+		city: decodeURIComponent(/** @type {string} */ (event.request.headers.get('x-vercel-ip-city'))),
+        country: decodeURIComponent(/** @type {string} */ (event.request.headers.get('x-vercel-ip-country'))),
+        region: decodeURIComponent(/** @type {string} */ (event.request.headers.get('x-vercel-ip-country-region'))),
+        latitude: decodeURIComponent(/** @type {string} */ (event.request.headers.get('x-vercel-ip-latitude'))),
+        longitude: decodeURIComponent(/** @type {string} */ (event.request.headers.get('x-vercel-ip-longitude'))),
+        timezone: decodeURIComponent(/** @type {string} */ (event.request.headers.get('x-vercel-ip-timezone')))
       
     }
   } catch (err) {
