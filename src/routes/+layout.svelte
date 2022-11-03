e28aa270cc5d1f48749a7e93ab4e70a7bb2b1399
carlosg2 <212313+carlosg2@users.noreply.github.com>
2022-11-03 18:02:50 -0500

    footer back

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -34,7 +34,7 @@
   </div>
 {/key} -->
 
-<!-- <Footer /> -->
+<Footer />
 
 <!-- <p class="w-full text-sm opacity-30 text-center p-4">{data.ip} / {data.city} / {data.country} / {data.region} / {data.latitude},{data.longitude} /{data.timezone}</p> -->
 
